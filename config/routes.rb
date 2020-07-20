192f229f3272098db257f74ca177a1daff3ab05f
shuuto.sugawara <0212shuto@gmail.com>
2020-07-20 05:54:23 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
   get 'users/new'
   root to: "tasks#index"
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   
   resources :tasks
   
