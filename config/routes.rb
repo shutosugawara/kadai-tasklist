fe7bfc5c6af6f5b813292eaac74c28f4a99b4520
shuuto.sugawara <0212shuto@gmail.com>
2020-07-20 05:17:11 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root to: "tasks#index"
   
   resources :tasks
+  
+  get "signup" , to: "users#new"
+  resources :users, only: [:new, :create]
 end
