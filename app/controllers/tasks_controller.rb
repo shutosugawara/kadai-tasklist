ed01758e0892fc519679125d4ef04e109397385b
shuuto.sugawara <0212shuto@gmail.com>
2020-07-20 12:10:47 +0000

    レッスン15課題

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
 
   def show
@@ -14,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に記録されました'
